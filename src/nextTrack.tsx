e6e434000f3953011a8da01ca8cee8f0ed4c0c95
SHEZ <shezcode@gmail.com>
2023-05-24 14:11:17 +0200

    added scripts

---
+++ src/nextTrack.tsx
@@ -0,0 +1,7 @@
+import { buildScriptEnsuringSpotifyIsRunning, runAppleScriptSilently } from "./utils";
+
+export default async () => {
+    const script = buildScriptEnsuringSpotifyIsRunning(`next track`)
+    await runAppleScriptSilently(script)
+}
+
