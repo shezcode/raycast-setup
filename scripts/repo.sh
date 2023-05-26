b18d609772a24eede99c0eb604a7f353fa837907
SHEZ <shezcode@gmail.com>
2023-05-26 12:32:12 +0200

    repo

---
+++ scripts/repo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title new repo 
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤑 
+
+# Documentation:
+# @raycast.author shezcode
+# @raycast.authorURL https://raycast.com/shezcode
+
+open "https://github.com/new"
