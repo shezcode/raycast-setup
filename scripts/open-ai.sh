e8126305ea61a3ccac757841c91bce362e09ab7f
SHEZ <shezcode@gmail.com>
2023-05-26 12:08:03 +0200

    scripts

---
+++ scripts/open-ai.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title GPT 
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤑 
+
+# Documentation:
+# @raycast.author shezcode
+# @raycast.authorURL https://raycast.com/shezcode
+
+open "https://chat.openai.com"
---
+++ scripts/open-gmail.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open Gmail
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 📪 
+
+# Documentation:
+# @raycast.author shezcode
+# @raycast.authorURL https://raycast.com/shezcode
+
+open "https://mail.google.com"
