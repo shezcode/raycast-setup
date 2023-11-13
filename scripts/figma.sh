283cfc51cdf89ad55165f4cd6ef8498fdd2a8f55
SHEZ <shezcode@gmail.com>
2023-11-13 14:36:40 +0100

    new scripts

---
+++ scripts/figma.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Figma
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 📪 
+
+# Documentation:
+# @raycast.author shezcode
+# @raycast.authorURL https://raycast.com/shezcode
+
+open "https://figma.com"
---
+++ scripts/jira.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Jira 
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 📊 
+
+# Documentation:
+# @raycast.author shezcode
+# @raycast.authorURL https://raycast.com/shezcode
+
+open "https://shezcode.atlassian.net/jira/projects"
